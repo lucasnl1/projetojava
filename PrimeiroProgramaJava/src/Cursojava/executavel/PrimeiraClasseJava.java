8e035d66ae263dfdadf88348414667d46d4921e5
Lucas <Lucas@DESKTOP-N199NOR>
2023-12-21 22:46:43 -0300

    Alteração

--- PrimeiroProgramaJava/src/Cursojava/executavel/PrimeiraClasseJava.java
+++ PrimeiroProgramaJava/src/Cursojava/executavel/PrimeiraClasseJava.java
@@ -4,6 +4,5 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa em java do curso de jdev!!");
 		System.out.println("Java é orientado a objeto");
-		System.out.println("outra alteração de teste");
 	}
 }
